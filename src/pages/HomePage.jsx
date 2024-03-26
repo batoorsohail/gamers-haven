794dcb0e8852f86f96c93958d27e3d1d76b35bd4
Ahmad Sohail <sohailshahidi1435@gmail.com>
2024-03-26 19:31:06 +0430

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+const HomePage = () => (
+  <section className="flex items-center justify-center h-screen bg-blue-400">
+    <h1 className="font-bold text-3xl text-white">Hello World</h1>
+  </section>
+);
+
+export default HomePage;
