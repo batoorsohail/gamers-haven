fb0569a6bac128dc9f4852480a71f66ee5f3c9cb
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-26 20:55:22 +0430

    Add Header.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 const HomePage = () => (
-  <section className="flex items-center justify-center h-screen bg-blue-400">
+  <section className="flex items-center justify-center h-screen">
     <h1 className="font-bold text-3xl text-white">Hello World</h1>
   </section>
 );
