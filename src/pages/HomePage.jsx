4d647557cddf52c74826620bbf5e4f147b3b21cd
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-03-30 15:02:01 +0430

    Display fetched data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
   }, [dispatch]);
 
   return (
-    <section className="flex flex-col gap-4 h-[80vh]">
+    <main className="flex flex-col gap-4 h-[80vh]">
       <h2 className="font-bold text-7xl text-white">All Games</h2>
       <div className="flex gap-5">
         <button type="button" className="bg-gray px-3 py-1 rounded-md text-white text-gray-hover slow-transition">
@@ -22,7 +22,15 @@ const HomePage = () => {
         <button type="button" className="bg-gray px-3 py-1 rounded-md text-white text-gray-hover slow-transition">Release Date</button>
         <button type="button" className="bg-gray px-3 py-1 rounded-md text-white text-gray-hover slow-transition">Platforms</button>
       </div>
-    </section>
+      <section className="flex flex-wrap">
+        {games.map((game) => (
+          <div key={game.gameId} className="w-60">
+            <img src={game.gameBg} alt={game.gameName}  className="w-[90%]"/>
+            <h3 className="text-white text-xl font-bold">{game.gameName}</h3>
+          </div>
+        ))}
+      </section>
+    </main>
   )
 };
 
