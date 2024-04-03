296df5beb3125438d81890daff4cb4595576fe8d
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-04-03 22:44:14 +0430

    Fix Linter error

--- src/pages/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage.jsx
@@ -24,35 +24,50 @@ const GameDetailsPage = () => {
             <div className="flex gap-5">
               {
                 gameData.ratings && Object.entries(gameData.ratings).map(([key, rating]) => {
-                  if (rating.title === "exceptional") {
+                  if (rating.title === 'exceptional') {
                     return (
                       <div key={key} className="flex gap-1 items-center">
-                        <div className="w-3 h-3 rounded-full bg-green-600"></div>
-                        <p className="text-gray text-base"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
+                        <div className="w-3 h-3 rounded-full bg-green-600" />
+                        <p className="text-gray text-base">
+                          <span className="text-white font-bold">{rating.title}</span>
+                          {' '}
+                          {rating.count}
+                        </p>
                       </div>
-                    )
-                  } else if (rating.title === "recommended") {
+                    );
+                  } if (rating.title === 'recommended') {
                     return (
                       <div key={key} className="flex gap-1 items-center">
-                        <div className="w-3 h-3 rounded-full bg-blue-600"></div>
-                        <p className="text-gray text-base"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
+                        <div className="w-3 h-3 rounded-full bg-blue-600" />
+                        <p className="text-gray text-base">
+                          <span className="text-white font-bold">{rating.title}</span>
+                          {' '}
+                          {rating.count}
+                        </p>
                       </div>
-                    )
-                  } else if (rating.title === "meh") {
+                    );
+                  } if (rating.title === 'meh') {
                     return (
                       <div key={key} className="flex gap-1 items-center">
-                        <div className="w-3 h-3 rounded-full bg-yellow-600"></div>
-                        <p className="text-gray text-base"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
+                        <div className="w-3 h-3 rounded-full bg-yellow-600" />
+                        <p className="text-gray text-base">
+                          <span className="text-white font-bold">{rating.title}</span>
+                          {' '}
+                          {rating.count}
+                        </p>
                       </div>
-                    )
-                  } else {
-                    return (
-                      <div key={key} className="flex gap-1 items-center">
-                        <div className="w-3 h-3 rounded-full bg-red-600"></div>
-                        <p className="text-gray text-base"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
-                      </div>
-                    )
+                    );
                   }
+                  return (
+                    <div key={key} className="flex gap-1 items-center">
+                      <div className="w-3 h-3 rounded-full bg-red-600" />
+                      <p className="text-gray text-base">
+                        <span className="text-white font-bold">{rating.title}</span>
+                        {' '}
+                        {rating.count}
+                      </p>
+                    </div>
+                  );
                 })
               }
             </div>
