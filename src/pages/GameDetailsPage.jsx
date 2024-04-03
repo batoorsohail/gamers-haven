dfcdbfc44632c01acd8629137cfe34b81352a9a6
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-04-03 22:39:42 +0430

    Add GamesDetails

--- src/pages/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage.jsx
@@ -26,34 +26,72 @@ const GameDetailsPage = () => {
                 gameData.ratings && Object.entries(gameData.ratings).map(([key, rating]) => {
                   if (rating.title === "exceptional") {
                     return (
-                      <div key={rating.key} className="flex gap-1 items-center">
+                      <div key={key} className="flex gap-1 items-center">
                         <div className="w-3 h-3 rounded-full bg-green-600"></div>
                         <p className="text-gray text-base"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
                       </div>
                     )
                   } else if (rating.title === "recommended") {
                     return (
-                      <div key={rating.key} className="flex gap-1 items-center">
+                      <div key={key} className="flex gap-1 items-center">
                         <div className="w-3 h-3 rounded-full bg-blue-600"></div>
                         <p className="text-gray text-base"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
                       </div>
                     )
                   } else if (rating.title === "meh") {
                     return (
-                      <div key={rating.key} className="flex gap-1 items-center">
+                      <div key={key} className="flex gap-1 items-center">
                         <div className="w-3 h-3 rounded-full bg-yellow-600"></div>
                         <p className="text-gray text-base"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
                       </div>
                     )
                   } else {
                     return (
-                      <div key={rating.key} className="flex gap-1 items-center">
+                      <div key={key} className="flex gap-1 items-center">
                         <div className="w-3 h-3 rounded-full bg-red-600"></div>
                         <p className="text-gray text-base"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
                       </div>
                     )
                   }
                 })
+              }
+            </div>
+          </div>
+          <div>
+            <div>
+              <p className="text-gray font-bold">Platforms</p>
+              {
+                gameData.platforms && Object.entries(gameData.platforms).map(([key, platform]) => (
+                  <p key={key} className="text-white">{platform.platform.name},</p>
+                ))
+              }
+            </div>
+            <div>
+              <p className="text-gray font-bold">Genre</p>
+              {
+                gameData.genres && Object.entries(gameData.genres).map(([key, genre]) => (
+                  <p key={key} className="text-white">{genre.name}</p>
+                ))
+              }
+            </div>
+            <div>
+              <p className="text-gray font-bold">Release Date</p>
+              <p className="text-white">{gameData.released}</p>
+            </div>
+            <div>
+              <p className="text-gray font-bold">Developers</p>
+              {
+                gameData.developers && Object.entries(gameData.developers).map(([key, developer]) => (
+                  <p key={key} className="text-white">{developer.name}</p>
+                ))
+              }
+            </div>
+            <div>
+              <p className="text-gray font-bold">Publishers</p>
+              {
+                gameData.publishers && Object.entries(gameData.publishers).map(([key, publisher]) => (
+                  <p key={key} className="text-white">{publisher.name}</p>
+                ))
               }
             </div>
           </div>
