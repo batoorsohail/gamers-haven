ef3a799fdaa8edf2a1728de608754048ceb34533
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-04-03 21:31:56 +0430

    Add rating counter

--- src/pages/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage.jsx
@@ -13,17 +13,20 @@ const GameDetailsPage = () => {
     dispatch(getGameDetails(gameId));
   }, [dispatch, gameId]);
 
-  console.log(gameData);
-
   return (
     <section>
-      <div className="flex flex-col w-3/5">
+      <div className="flex flex-col gap-5 w-3/4 mx-10 pb-10">
         <img src={gameData.background_image} alt={gameData.name} className="bg-local" />
-        {
-          gameData.ratings.map((rating) => {
-           <p className="text-gray"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
-          })
-        }
+        <p className="text-white text-xl font-bold">Ratings:</p>
+        <div className="flex gap-5">
+          {
+            gameData.ratings && Object.entries(gameData.ratings).map(([key, rating]) => (
+            <div key={rating.key}>
+              <p className="text-gray text-lg"><span className="text-white font-bold">{rating.title}</span> {rating.count}</p>
+            </div>
+            ))
+          }
+        </div>
         <div>
           <h2 className="text-white text-3xl font-bold">{gameData.name}</h2>
           <p className="text-white text-md">{gameData.description_raw}</p>
