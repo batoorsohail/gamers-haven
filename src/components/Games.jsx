ea7ac122242ae7ab82cd932aa56564cc5623ef04
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-04-08 20:21:14 +0430

    Refactor HomePage.jsx file

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
-const Games = () => {
+const Games = ({games}) => {
   return (
-    <div>Games</div>
+    <section className="flex flex-wrap gap-5">
+        {games.map((game) => (
+          <div key={game.gameId} className="lg:w-72 w-2/5 rounded-xl bg-gray shadow-lg">
+            <Link to={`games/${game.gameId}`}>
+              <img src={game.gameBg} alt={game.gameName} className="w-full rounded-xl" />
+              <div className="flex flex-col gap-2 px-3 pb-5 pt-2">
+                <h3 className="text-white text-xl font-bold">{game.gameName}</h3>
+                <p className="flex justify-between text-gray">
+                  Release Date:
+                  <span className="text-white">{game.gameReleased}</span>
+                </p>
+                <p className="flex justify-between text-gray">
+                  Game Ratings:
+                  <span className="text-white">
+                    {game.gameRating}
+                    /5
+                  </span>
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </section>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import Games from '../components/Games';
 import { getGames, selectAllGames } from '../redux/games/gamesSlice';
 
 const HomePage = () => {
@@ -31,29 +31,7 @@ const HomePage = () => {
         <button type="button" className="bg-gray px-3 py-1 rounded-md text-white text-gray-hover slow-transition">Release Date</button>
         <button type="button" className="bg-gray px-3 py-1 rounded-md text-white text-gray-hover slow-transition">Platforms</button>
       </div>
-      <section className="flex flex-wrap gap-5">
-        {games.map((game) => (
-          <div key={game.gameId} className="lg:w-72 w-2/5 rounded-xl bg-gray shadow-lg">
-            <Link to={`games/${game.gameId}`}>
-              <img src={game.gameBg} alt={game.gameName} className="w-full rounded-xl" />
-              <div className="flex flex-col gap-2 px-3 pb-5 pt-2">
-                <h3 className="text-white text-xl font-bold">{game.gameName}</h3>
-                <p className="flex justify-between text-gray">
-                  Release Date:
-                  <span className="text-white">{game.gameReleased}</span>
-                </p>
-                <p className="flex justify-between text-gray">
-                  Game Ratings:
-                  <span className="text-white">
-                    {game.gameRating}
-                    /5
-                  </span>
-                </p>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </section>
+      <Games games={games}/>
     </main>
   );
 };
